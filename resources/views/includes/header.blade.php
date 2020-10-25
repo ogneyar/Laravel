3733d73a8ee1b4652259d8098647eceb063dc1b5
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-10-25 14:40:36 +0300

    Add files via upload

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,16 +1,3 @@
-<? 
-$tester = getenv('TESTER');
-if ($tester == 'да') {
-?>
-<a href="/" id="logo">
-	<b><span>TESTMarket</span></b>
-</a>
-<?
-}else {
-?>
 <a href="/" id="logo">
 	<b><span>PRIZMarket</span></b>
 </a>
-<?
-} 
-?>
