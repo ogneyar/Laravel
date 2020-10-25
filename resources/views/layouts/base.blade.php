eb3e18957c379b258ebc87e1481d82fd418496b0
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-10-25 13:50:03 +0300

    Add files via upload

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>@yeild('title')</title>
+        <title>@yield('title')</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -20,7 +20,7 @@
         </style>
     </head>
 		<div>
-		@yeild('content')
+		@yield('content')
 		</div>
 		
 		@include('includes.aside')
