d6bc9e4c040ec86ab3013da440d1a8f10dbe2264
ogneyar <ogneyar-ne@ya.ru>
2020-10-27 16:37:00 +0300

    ccccccc

--- resources/views/support/support.blade.php
+++ resources/views/support/support.blade.php
@@ -5,6 +5,10 @@
 @endsection
 
 @section('content')
-<h1>Тех поддержка</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore expedita atque officiis mollitia distinctio, officia itaque magnam corporis unde facere! Deserunt fuga velit tenetur at officiis, voluptate eaque aliquid repudiandae!</p>
+<article id="support">
+    <h1><b class="b_blue">Команда PRIZMarket</b> трудится во благо Вам и для развития криптовалюты PRIZM.</h1>    
+    <p>Обращение в <b class="b_green">ТехПоддержку</b> пока что возможно только через <a href="https://t.me/Prizm_market_supportbot" target="_blank">телеграм</a>.</p>
+    <p>Для этого перейдите по ссылке - <a href="https://t.me/Prizm_market_supportbot" target="_blank">Supportbot</a>.</p>
+    <p>Опишите свои трудности боту подробно, администраторы Вам обязательно помогут.</p>
+</article>
 @endsection
