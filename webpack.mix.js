feb1c23d5b55b18a995db4fd3c48cd98d84bb61d
ogneyar <ogneyar-ne@ya.ru>
2020-11-07 23:28:13 +0300

    scss instead sass

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,4 +8,4 @@ mix.js('resources/js/app.js', 'public/js')
 */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/scss/app.scss', 'public/css');
