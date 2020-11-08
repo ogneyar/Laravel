2b34eba375a3646a7823aa5f8eac4f4b59da616e
Ogneyar <56074933+ogneyar@users.noreply.github.com>
2020-11-08 23:07:19 +0300

    Add files via upload

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,4 +8,25 @@ mix.js('resources/js/app.js', 'public/js')
 */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/scss/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css');
+	
+/*
+mix.browserSync({
+	baseDir: "/public",
+    index: "index.html",
+	port: 8000,
+	//startPath: "/index.html",
+	//host: "192.168.1.1",
+	proxy: "localhost:8080"
+});	
+*/
+
+//mix.disableNotifications();
+
+// npm run hot
+mix.options({
+    hmrOptions: {
+        host: 'localhost',
+        port: 8000
+    }
+});
